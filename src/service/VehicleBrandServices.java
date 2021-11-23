36b15c085ff900d606d8337218ec2a43f812ac00
Alejandro <alejocuba2014@gmail.com>
2021-11-22 21:21:01 -0500

    Fixing services

--- src/service/VehicleBrandServices.java
+++ src/service/VehicleBrandServices.java
@@ -31,16 +31,16 @@ public class VehicleBrandServices implements Services<VehicleBrandDto> {
     @Override
     public List<VehicleBrandDto> loadAll() throws SQLException {
 
-        LinkedList<VehicleBrandDto> vehicleBrandlist=new LinkedList<VehicleBrandDto>();
+        LinkedList<VehicleBrandDto> vehicleBrandlist = new LinkedList<VehicleBrandDto>();
         Connection connection = ServicesLocator.getConnection();
         connection.setAutoCommit(false);
 
         CallableStatement callableStatement = connection.prepareCall("{? = call tpp.n_vehicle_brand_load}");
         callableStatement.registerOutParameter(1, Types.REF_CURSOR);
         callableStatement.execute();
-        ResultSet resultSet=(ResultSet) callableStatement.getObject(1);
+        ResultSet resultSet = (ResultSet) callableStatement.getObject(1);
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             vehicleBrandlist.add(new VehicleBrandDto(resultSet.getInt("id_vehicle_brand"),
                     resultSet.getString("name")));
         }
@@ -51,15 +51,28 @@ public class VehicleBrandServices implements Services<VehicleBrandDto> {
     @Override
     public void insert(VehicleBrandDto dto) throws SQLException {
 
+        Connection connection = ServicesLocator.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{call tpp.n_vehicle_brand_insert(?)}");
+        callableStatement.setString(1, dto.getName());
+        callableStatement.execute();
     }
 
     @Override
     public void update(VehicleBrandDto dto) throws SQLException {
+        Connection connection = ServicesLocator.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{call tpp.n_vehicle_brand_update(?,?)}");
+        callableStatement.setInt(1, dto.getId());
+        callableStatement.setString(2, dto.getName());
+        callableStatement.execute();
 
     }
 
     @Override
     public void delete(int id) throws SQLException {
+        Connection connection = ServicesLocator.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{call tpp.n_vehicle_brand_update(?)}");
+        callableStatement.setInt(1, id);
+        callableStatement.execute();
 
     }
 
--- src/service/VehicleServices.java
+++ src/service/VehicleServices.java
@@ -1,6 +1,5 @@
 package service;
 
-import dto.ContractDto;
 import dto.VehicleDto;
 import dto.nom.VehicleBrandDto;
 
@@ -12,24 +11,7 @@ import java.util.List;
 public class VehicleServices implements Services<VehicleDto>, Relation<VehicleDto>{
     @Override
     public VehicleDto load(int id) throws SQLException {
-        Connection connection = ServicesLocator.getConnection();
-        connection.setAutoCommit(false);
-        CallableStatement callableStatement = connection.prepareCall("{? = call tpp.vehicle_load_by_id(?)}");
-        callableStatement.registerOutParameter(1, Types.REF_CURSOR);
-        callableStatement.setInt(2, id);
-        callableStatement.execute();
-        ResultSet resultSet = (ResultSet) callableStatement.getObject(1);
-        ContractDto contractDto = null;
-        resultSet.next();
-
-        return new VehicleDto(
-                resultSet.getInt("id_vehicle"),
-                resultSet.getString("chapa"),
-                ServicesLocator.getVehicleBrandServices().load(resultSet.getInt("id_vehicle_brand")),
-                resultSet.getInt("capacity_without_baggage"),
-                resultSet.getInt("capacity_with_baggage"),
-                resultSet.getDate("production_date").toLocalDate()
-        );
+        return null;
     }
 
     @Override
@@ -64,7 +46,20 @@ public class VehicleServices implements Services<VehicleDto>, Relation<VehicleDt
 
         Connection connection = ServicesLocator.getConnection();
         CallableStatement callableStatement = connection.prepareCall("{call tpp.vehicle_insert(?,?,?,?,?,?)}");
-        callableStatement.setInt(1, dto.getId());
+        callableStatement.setString(1, dto.getRegistration());
+        callableStatement.setInt(2, dto.getCapacityWithoutBaggage());
+        callableStatement.setInt(3, dto.getCapacityWithBaggage());
+        callableStatement.setInt(4, dto.getCapacityTotal());
+        callableStatement.setDate(5, dto.toDate());
+        callableStatement.setInt(6, dto.getBrand().getId());
+        callableStatement.execute();
+    }
+
+    @Override
+    public void update(VehicleDto dto) throws SQLException {
+        Connection connection = ServicesLocator.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{call tpp.vehicle_insert(?,?,?,?,?,?,?)}");
+        callableStatement.setInt(1,dto.getId());
         callableStatement.setString(2, dto.getRegistration());
         callableStatement.setInt(3, dto.getCapacityWithoutBaggage());
         callableStatement.setInt(4, dto.getCapacityWithBaggage());
@@ -72,15 +67,15 @@ public class VehicleServices implements Services<VehicleDto>, Relation<VehicleDt
         callableStatement.setDate(6, dto.toDate());
         callableStatement.setInt(7, dto.getBrand().getId());
         callableStatement.execute();
-    }
-
-    @Override
-    public void update(VehicleDto dto) throws SQLException {
 
     }
 
     @Override
     public void delete(int id) throws SQLException {
+        Connection connection = ServicesLocator.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{call tpp.vehicle_insert(?)}");
+        callableStatement.setInt(1,id);
+        callableStatement.execute();
 
     }
 
@@ -99,13 +94,20 @@ public class VehicleServices implements Services<VehicleDto>, Relation<VehicleDt
         callableStatement.setInt(2,id);
         callableStatement.execute();
         ResultSet resultSet= (ResultSet) callableStatement.getObject(1);
-        int idVehicle;
-        VehicleDto vehicleDto;
 
         while (resultSet.next()){
-            idVehicle = resultSet.getInt("id_vehicle");
-            vehicleDto = load(idVehicle);
-            vehicleDtoLinkedList.add(vehicleDto);
+            String name=ServicesLocator.getVehicleBrandServices().load(resultSet.getInt(2)).getName();
+            LocalDate localDate=resultSet.getDate("production_date").toLocalDate();
+            vehicleDtoLinkedList.add(
+                    new VehicleDto(
+                            resultSet.getInt("id_vehicle"),
+                            resultSet.getString("id_chapa"),
+                            new VehicleBrandDto(resultSet.getInt("id_vehicle_brand"),name),
+                            resultSet.getInt("capacity_without_baggage"),
+                            resultSet.getInt("capacity_with_baggage"),
+                            localDate
+
+            ));
         }
         return vehicleDtoLinkedList;
     }
