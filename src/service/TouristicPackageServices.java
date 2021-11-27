11bacbce5e9847113ddac3d7a90f003e3561513a
lissy <lissybouza@gmail.com>
2021-11-27 01:16:27 -0500

    touristicPackage loadAll

--- src/service/TouristicPackageServices.java
+++ src/service/TouristicPackageServices.java
@@ -1,6 +1,10 @@
 package service;
 
+import dto.ContractDto;
 import dto.TouristicPackageDto;
+import dto.nom.FoodPlanDto;
+import dto.nom.ModalityCommercialDto;
+import dto.nom.RoomTypeDto;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -15,10 +19,13 @@ public class TouristicPackageServices implements Services<TouristicPackageDto>{
         callableStatement.registerOutParameter(1, Types.REF_CURSOR);
         callableStatement.setInt(2, id);
         callableStatement.execute();
+
         ResultSet resultSet = (ResultSet) callableStatement.getObject(1);
         resultSet.next();
+
         callableStatement.close();
         connection.close();
+
         return new TouristicPackageDto(
                 resultSet.getInt("id_touristic_package"),
                 resultSet.getString("promotionla_name"),
@@ -37,31 +44,56 @@ public class TouristicPackageServices implements Services<TouristicPackageDto>{
     @Override
     public List<TouristicPackageDto> loadAll() throws SQLException {
         List<TouristicPackageDto> touristicPackageDtos = new LinkedList<>();
+        TouristicPackageDto touristicPackageDto;
+        ContractDto contractDto;
         Connection connection = ServicesLocator.getConnection();
+        int idTouristicPackage;
+        int idContract;
+        List<Integer> idContainerTouristicPackage = new LinkedList<>();
+        List<Integer> idContainerContract = new LinkedList<>();
+
         connection.setAutoCommit(false);
         CallableStatement callableStatement = connection.prepareCall("{? = call tpp.touristic_package_load()}");
         callableStatement.registerOutParameter(1, Types.REF_CURSOR);
         callableStatement.execute();
         ResultSet resultSet = (ResultSet) callableStatement.getObject(1);
-        int idTourPack;
+
 
         while (resultSet.next()) {
-            idTourPack = resultSet.getInt("id_touristic_package");
-            touristicPackageDtos.add(new TouristicPackageDto(
-                    idTourPack,
-                    resultSet.getString("promotional_name"),
-                    resultSet.getInt("days_amount"),
-                    resultSet.getInt("nights_amount"),
-                    resultSet.getInt("pax_amount"),
-                    resultSet.getFloat("cost_hotel"),
-                    resultSet.getFloat("cost_transport"),
-                    resultSet.getFloat("cost_transport_hotel_airport"),
-                    resultSet.getFloat("cost_total"),
-                    resultSet.getFloat("price"),
-                    ServicesLocator.getContractServices().loadRelated(idTourPack)
-            ) {
-            });
-        }
+            idTouristicPackage = resultSet.getInt("id_touristic_package");
+            idContract = resultSet.getInt("id_contract");
+
+            if (!idContainerTouristicPackage.contains(idTouristicPackage)) {
+                idContainerTouristicPackage.add(idTouristicPackage);
+                idContainerContract.clear();
+                contractDto = ServicesLocator.getContractServices().load(idContract);
+                List<ContractDto> ListContractInsert = new LinkedList<>();
+                ListContractInsert.add(contractDto);
+                idContainerContract.add(idContract);
+
+                touristicPackageDtos.add(new TouristicPackageDto(
+                        idTouristicPackage,
+                        resultSet.getString("promotional_name"),
+                        resultSet.getInt("days_amount"),
+                        resultSet.getInt("nights_amount"),
+                        resultSet.getInt("pax_amount"),
+                        resultSet.getFloat("cost_hotel"),
+                        resultSet.getFloat("cost_transport"),
+                        resultSet.getFloat("cost_transport_hotel_airport"),
+                        resultSet.getFloat("cost_total"),
+                        resultSet.getFloat("price"),
+                        ListContractInsert
+                ));
+            } else {
+                int size = touristicPackageDtos.size();
+                touristicPackageDto = touristicPackageDtos.get(size - 1);
+
+                if (!idContainerContract.contains(idContract)) {
+                    touristicPackageDto.getContracts().add(ServicesLocator.getContractServices().load(idContract));
+                    idContainerContract.add(idContract);
+                }
+            }
+
 
         callableStatement.close();
         connection.close();
