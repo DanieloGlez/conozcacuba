15e24ecbc44d8cddfb0f42dacdbe7fc974492122
Ernesto <em756691@gmail.com>
2021-11-27 20:14:16 -0500

    Optimized method load in HotelServices

--- src/service/HotelServices.java
+++ src/service/HotelServices.java
@@ -1,9 +1,7 @@
 package service;
 
 import dto.HotelDto;
-import dto.nom.FoodPlanDto;
-import dto.nom.ModalityCommercialDto;
-import dto.nom.RoomTypeDto;
+import dto.nom.*;
 
 import java.sql.*;
 import java.util.LinkedList;
@@ -69,7 +67,7 @@ public class HotelServices implements Services<HotelDto> {
         connection.close();
         return hotelDto;
     }
-
+/*
     @Override
     public List<HotelDto> loadAll() throws SQLException {
         List<HotelDto> ListHotelDtos = new LinkedList<>();
@@ -158,7 +156,152 @@ public class HotelServices implements Services<HotelDto> {
         callableStatement.close();
         connection.close();
         return ListHotelDtos;
+    }*/
+
+    @Override
+    public List<HotelDto> loadAll() throws SQLException {
+        List<HotelDto> ListHotelDtos = new LinkedList<>();
+        HotelDto hotelDto = null;
+        RoomTypeDto roomTypeDto;
+        FoodPlanDto foodPlanDto;
+        ModalityCommercialDto modalityCommercialDto;
+        Connection connection = ServicesLocator.getConnection();
+        int idHotel;
+        int idRoomType;
+        int idFoodPlan;
+        int idModalities;
+        List<Integer> idContainerHotel = new LinkedList<>();
+        List<Integer> idContainerRoomType = new LinkedList<>();
+        List<Integer> idContainerFoodPlan = new LinkedList<>();
+        List<Integer> idContainerModalities = new LinkedList<>();
+
+        connection.setAutoCommit(false);
+        CallableStatement callableStatement = connection.prepareCall("{? = call tpp.hotel_load()}");
+        callableStatement.registerOutParameter(1, Types.REF_CURSOR);
+        callableStatement.execute();
+        ResultSet resultSet = (ResultSet) callableStatement.getObject(1);
+
+        while (resultSet.next()) {
+            idHotel = resultSet.getInt("id_hotel");
+            idRoomType = resultSet.getInt("id_room_type");
+            idFoodPlan = resultSet.getInt("id_food_plan");
+            idModalities = resultSet.getInt("id_modality_hotel_comertial");
+
+            if (!idContainerHotel.contains(idHotel)) {//inserto los elementos de la primera aparicion de un identificador
+                idContainerHotel.add(idHotel);
+                idContainerRoomType.clear();
+                idContainerFoodPlan.clear();
+                idContainerModalities.clear();
+                roomTypeDto = ServicesLocator.getRoomTypeServices().load(idRoomType);
+                foodPlanDto = ServicesLocator.getFoodPlanServices().load(idFoodPlan);
+                modalityCommercialDto = ServicesLocator.getModalityCommercialServices().load(idModalities);
+                List<RoomTypeDto> ListRoomTypeInsert = new LinkedList<>();
+                List<FoodPlanDto> ListFoodPlanInsert = new LinkedList<>();
+                List<ModalityCommercialDto> ListModalityCommercialInsert = new LinkedList<>();
+                ListRoomTypeInsert.add(roomTypeDto);
+                ListFoodPlanInsert.add(foodPlanDto);
+                ListModalityCommercialInsert.add(modalityCommercialDto);
+                idContainerRoomType.add(idRoomType);
+                idContainerFoodPlan.add(idFoodPlan);
+                idContainerModalities.add(idModalities);
+
+                CallableStatement callableStatementHF = connection.prepareCall("{? = call tpp.n_hotel_franchise_load_by_id(?)}");
+                callableStatementHF.registerOutParameter(1, Types.REF_CURSOR);
+                int idHotelFranchise = resultSet.getInt("id_hotel_franchise");
+                callableStatementHF.setInt(2, idHotelFranchise);
+                callableStatementHF.execute();
+                ResultSet resultSetHF = (ResultSet) callableStatementHF.getObject(1);
+                resultSetHF.next();
+                HotelFranchiseDto hotelFranchiseDto = new HotelFranchiseDto(idHotelFranchise, resultSetHF.getString("name"));
+                callableStatementHF.close();
+
+                CallableStatement callableStatementPr = connection.prepareCall("{? = call tpp.n_province_load_by_id(?)}");
+                callableStatementPr.registerOutParameter(1, Types.REF_CURSOR);
+                int idProvince = resultSet.getInt("id_province");
+                callableStatementPr.setInt(2, idProvince);
+                callableStatementPr.execute();
+                ResultSet resultSetPr = (ResultSet) callableStatementPr.getObject(1);
+                resultSetPr.next();
+                ProvinceDto provinceDto = new ProvinceDto(idProvince, resultSetPr.getString("name"));
+                callableStatementPr.close();
+
+                CallableStatement callableStatementLoc = connection.prepareCall("{? = call tpp.n_localization_load_by_id(?)}");
+                callableStatementLoc.registerOutParameter(1, Types.REF_CURSOR);
+                int idLocalization = resultSet.getInt("id_hotel_franchise");
+                callableStatementLoc.setInt(2, idLocalization);
+                callableStatementLoc.execute();
+                ResultSet resultSetLoc = (ResultSet) callableStatementLoc.getObject(1);
+                resultSetLoc.next();
+                LocalizationDto localizationDto = new LocalizationDto(idLocalization, resultSetLoc.getString("name"));
+                callableStatementLoc.close();
+
+                hotelDto = new HotelDto(
+                        idHotel,
+                        resultSet.getString("name"),
+                        resultSet.getString("address"),
+                        resultSet.getString("category"),
+                        resultSet.getString("telephone_number"),
+                        resultSet.getString("fax"),
+                        resultSet.getString("email"),
+                        resultSet.getFloat("dist_to_city"),
+                        resultSet.getFloat("dist_to_airport"),
+                        resultSet.getInt("rooms_amount"),
+                        resultSet.getInt("floors_amount"),
+                        hotelFranchiseDto,
+                        provinceDto,
+                        localizationDto);
+
+                hotelDto.setRoomTypes(ListRoomTypeInsert);
+                hotelDto.setFoodPlans(ListFoodPlanInsert);
+                hotelDto.setCommercialModalities(ListModalityCommercialInsert);
+                ListHotelDtos.add(hotelDto);
+            } else {//inserto para el mismo id los tipos de hab, planes alim y modalidades diferentes
+                if (!idContainerRoomType.contains(idRoomType)) {
+                    CallableStatement callableStatementRoom = connection.prepareCall("{? = call tpp.n_room_type_load_by_id(?)}");
+                    callableStatementRoom.registerOutParameter(1, Types.REF_CURSOR);
+                    callableStatementRoom.setInt(2, idRoomType);
+                    callableStatementRoom.execute();
+                    ResultSet resultSetRoom = (ResultSet) callableStatementRoom.getObject(1);
+                    resultSetRoom.next();
+                    roomTypeDto = new RoomTypeDto(idRoomType, resultSetRoom.getString("name"));
+                    hotelDto.getRoomTypes().add(roomTypeDto);
+                    idContainerRoomType.add(idRoomType);
+                    callableStatementRoom.close();
+                }
+
+                if (!idContainerFoodPlan.contains(idFoodPlan)) {
+                    CallableStatement callableStatementFood = connection.prepareCall("{? = call tpp.n_food_plan_load_by_id(?)}");
+                    callableStatementFood.registerOutParameter(1, Types.REF_CURSOR);
+                    callableStatementFood.setInt(2, idFoodPlan);
+                    callableStatementFood.execute();
+                    ResultSet resultSetFood = (ResultSet) callableStatementFood.getObject(1);
+                    resultSetFood.next();
+                    foodPlanDto = new FoodPlanDto(idFoodPlan, resultSetFood.getString("name"));
+                    hotelDto.getFoodPlans().add(foodPlanDto);
+                    idContainerFoodPlan.add(idFoodPlan);
+                    callableStatementFood.close();
+                }
+
+                if (!idContainerModalities.contains(idModalities)) {
+                    CallableStatement callableStatementMod = connection.prepareCall("{? = call tpp.n_modality_hotel_comertial_load_by_id(?)}");
+                    callableStatementMod.registerOutParameter(1, Types.REF_CURSOR);
+                    callableStatementMod.setInt(2, idModalities);
+                    callableStatementMod.execute();
+                    ResultSet resultSetMod = (ResultSet) callableStatementMod.getObject(1);
+                    resultSetMod.next();
+                    modalityCommercialDto = new ModalityCommercialDto(idModalities, resultSetMod.getString("name"));
+                    hotelDto.getCommercialModalities().add(modalityCommercialDto);
+                    idContainerModalities.add(idModalities);
+                    callableStatementMod.close();
+                }
+            }
+        }
+
+        callableStatement.close();
+        connection.close();
+        return ListHotelDtos;
     }
+
 
     @Override
     public void insert(HotelDto dto) throws SQLException {
