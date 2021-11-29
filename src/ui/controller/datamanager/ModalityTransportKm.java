0bdf0addece1b8c69751ac881dfbf152a3ab1834
danielo <jose.daniel0523@gmail.com>
2021-11-29 00:16:13 -0500

    ModalityTransportKm Modal Implemented

--- src/ui/controller/datamanager/ModalityTransportKm.java
+++ src/ui/controller/datamanager/ModalityTransportKm.java
@@ -3,11 +3,16 @@ package ui.controller.datamanager;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import dto.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import service.ServicesLocator;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ModalityTransportKm extends DataManagerFormController {
@@ -21,24 +26,73 @@ public class ModalityTransportKm extends DataManagerFormController {
     private JFXTextField costhrwait_jfxtextfield;
 
     @FXML
-    private JFXComboBox<?> contract_jfxcombobox;
+    private JFXComboBox<ContractTransportDto> contract_jfxcombobox;
 
     @FXML
-    private JFXComboBox<?> vehicle_jfxcombobox;
+    private JFXComboBox<VehicleDto> vehicle_jfxcombobox;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        try {
+            contract_jfxcombobox.getItems().addAll(ServicesLocator.getContractTransportServices().loadAll());
+            vehicle_jfxcombobox.getItems().addAll(ServicesLocator.getVehicleServices().loadAll());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void insert(ActionEvent event) {
+    public void insert(ActionEvent event) throws SQLException {
+        ServicesLocator.getModalityTransportKmServices().insert(new ModalityTransportKmDto(
+                0,
+                Float.parseFloat(costkm_jfxtextfield.getText()),
+                Float.parseFloat(costkmroundtrip_jfxtextfield.getText()),
+                Float.parseFloat(costhrwait_jfxtextfield.getText()),
+                contract_jfxcombobox.getValue(),
+                vehicle_jfxcombobox.getValue()
+        ));
 
+        ((Stage) costkm_jfxtextfield.getScene().getWindow()).close();
     }
 
     @Override
-    public void update(ActionEvent event) {
+    public void update(ActionEvent event) throws SQLException {
+        ServicesLocator.getModalityTransportKmServices().update(new ModalityTransportKmDto(
+                dto.getId(),
+                Float.parseFloat(costkm_jfxtextfield.getText()),
+                Float.parseFloat(costkmroundtrip_jfxtextfield.getText()),
+                Float.parseFloat(costhrwait_jfxtextfield.getText()),
+                contract_jfxcombobox.getValue(),
+                vehicle_jfxcombobox.getValue()
+        ));
 
+        ((Stage) costkm_jfxtextfield.getScene().getWindow()).close();
+    }
+
+    @Override
+    public void setDto(Dto dto) {
+        super.setDto(dto);
+
+        if(dto != null) {
+            ModalityTransportKmDto modalityTransportKmDto = (ModalityTransportKmDto) dto;
+            costkm_jfxtextfield.setText(String.valueOf(modalityTransportKmDto.getCostKm()));
+            costkmroundtrip_jfxtextfield.setText(String.valueOf(modalityTransportKmDto.getCostKmRoundTrip()));
+            costhrwait_jfxtextfield.setText(String.valueOf(modalityTransportKmDto.getCostHrWait()));
+
+            for (ContractTransportDto contractTransportDto : contract_jfxcombobox.getItems()) {
+                if (contractTransportDto.getId() == modalityTransportKmDto.getContractTransport().getId()) {
+                    contract_jfxcombobox.getSelectionModel().select(contractTransportDto);
+                    break;
+                }
+            }
+
+            for (VehicleDto vehicleDto : vehicle_jfxcombobox.getItems()) {
+                if (vehicleDto.getId() == modalityTransportKmDto.getVehicle().getId()) {
+                    vehicle_jfxcombobox.getSelectionModel().select(vehicleDto);
+                    break;
+                }
+            }
+        }
     }
 }
